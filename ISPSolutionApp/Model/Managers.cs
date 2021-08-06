9622af31270584315af94b483dfd3363794851bf
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:22:20 +0530

    Add files via upload

---
+++ ISPSolutionApp/Model/Managers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPSolutionApp.Model
+{
+    class Managers : IWork, IEat
+    {
+        public void StartEat()
+        {
+            Console.WriteLine("Managers have started eating...");
+        }
+
+        public void StartWork()
+        {
+            Console.WriteLine("Managers have started working...");
+        }
+
+        public void StopEat()
+        {
+            Console.WriteLine("Managers have finished eating...");
+        }
+
+        public void StopWork()
+        {
+            Console.WriteLine("Managers have finished working...");
+        }
+    }
+}
---
+++ ISPSolutionApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISPSolutionApp.Model;
+
+namespace ISPSolutionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoAllWorkingJobs(new Managers());
+            DoAllEatingJobs(new Managers());
+            DoAllWorkingJobs(new Robots());
+            Console.ReadLine();
+        }
+        static void DoAllWorkingJobs(IWork i)
+        {
+            i.StartWork();
+            i.StopWork();
+        }
+
+        static void DoAllEatingJobs(IEat e)
+        {
+            e.StartEat();
+            e.StopEat();
+        }
+    }
+}
